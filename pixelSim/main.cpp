16b9689551d1fd70e437aa28ba5db830d9eb5575
Jacob <jdykma80992@viterbo.edu>
2023-11-11 01:06:42 -0600

    1.0.21
    Added gravity and a floor to the window so objects don't fall through. If an object's velocity = 0, it snaps to the grid.

--- pixelSim/main.cpp
+++ pixelSim/main.cpp
@@ -2,8 +2,7 @@
 #include <cmath>
 
 // Constants and globals
-const sf::Vector2f pixelSize(20.0f, 20.0f); //TODO this
-const sf::Time coolDown = sf::milliseconds(100); //TODO drag and draw
+const sf::Vector2f pixelSize(12.0f, 12.0f); //TODO this
 const float gravity = 9.81f;
 float deltaTime = 0.005667f;
 bool buttonL;
@@ -13,11 +12,13 @@ struct GameObject{
     float vx; float vy;
     float ax; float ay;
     float w; float h;
+    sf::Color c;
     int id;
 };
 
 std::vector<GameObject> objects;
 std::vector<std::pair<GameObject*, GameObject*>> vecCollidingPairs;
+
 GameObject *pSelectedObject = nullptr;
 void AddRect(float x, float y, float w = pixelSize.x, float h = pixelSize.y){
     GameObject b;
@@ -26,13 +27,14 @@ void AddRect(float x, float y, float w = pixelSize.x, float h = pixelSize.y){
     b.vx = 0; b.vy = 0;
     b.ax = 0; b.ay = 0;
     b.id = objects.size();
+
     objects.emplace_back(b);
 }
 
 // Functions
-void drawRectangleGameObject(GameObject& rect); // Draw a rect
+void drawRectangleGameObject(GameObject& rect, sf::Color color); // Draw a rect
 void drawLineBetween(float x1, float y1, float x2, float y2, sf::Color); // Draw a line between two points
-void snapGameObjectToGrid(GameObject& obj, float gridSize); //TODO Make this only happen if the object is not moving
+void snapGameObjectToGrid(GameObject& obj); //TODO Make this only happen if the object is not moving
 
 // Helper Functions
 void handleMouseButtonPressed(const sf::Event& event);
@@ -52,7 +54,7 @@ bool DoRectanglesOverlap(float px1, float py1, float width1, float height1, floa
     // Check for overlap
     return !(left1 > right2 || right1 < left2 || top1 > bottom2 || bottom1 < top2);
 }
-
+bool doGhostsOverlap(float px, float py, float checkWidth, float checkHeight, float targetX, float targetY);
 auto IsPointInRectangle = [](float rectX, float rectY, float width, float height, float px, float py) {
     // Calculate the left, right, top, and bottom of the rectangle
     float left = rectX - width / 2.0f;
@@ -63,55 +65,72 @@ auto IsPointInRectangle = [](float rectX, float rectY, float width, float height
     // Check if the point is within the bounds of the rectangle
     return (px >= left && px <= right) && (py >= top && py <= bottom);
 };
+bool isInCollidingPairs(GameObject *obj) { // Function to check if an object is in the vecCollidingPairs
+    for (const auto& pair : vecCollidingPairs) {
+        if (pair.first == obj || pair.second == obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 sf::RenderWindow window(sf::VideoMode(900, 500), "Pixel Engine"); // Create the game window
 
 
 int main()
 {
-    for (int i = 0; i < 10; ++i) { // Draw some pixels randomly
-        AddRect(rand() % window.getSize().x, rand() % window.getSize().y, 20);
+    for (int i = 0; i < 50; ++i) { // Draw some pixels randomly
+        float adjustedX = rand() % (int)std::round(window.getSize().x / pixelSize.x) * pixelSize.x;
+        float adjustedY = rand() % (int) std::round(window.getSize().y / pixelSize.y) * pixelSize.y;
+        AddRect(adjustedX, adjustedY);
+    }
+    for(auto obj : objects){
+        snapGameObjectToGrid(obj);
     }
     while (window.isOpen()) {
         sf::Event event{};
         vecCollidingPairs.clear(); // Clear colliding objects vector every frame
-
-
-
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
             else if (event.type == sf::Event::MouseButtonPressed) handleMouseButtonPressed(event);
             else if (event.type == sf::Event::MouseButtonReleased) handleMouseButtonReleased(event);
         }
         window.clear();
-
-        if(buttonL){
+        if (buttonL) {
             sf::Vector2i mousePos = sf::Mouse::getPosition(window);
             pSelectedObject->px = mousePos.x;
             pSelectedObject->py = mousePos.y;
         }
-        if(buttonR){
+        if (buttonR) {
             sf::Vector2i mousePos = sf::Mouse::getPosition(window);
             drawLineBetween(pSelectedObject->px, pSelectedObject->py, mousePos.x, mousePos.y, sf::Color::Cyan);
         }
-        for(auto &ball : objects){
-            ball.ax = -ball.vx * 0.3f;
-            ball.ay = -ball.vy * 0.3f;
-            ball.vx += ball.ax * deltaTime;
-            ball.vy += ball.ay * deltaTime;
-            ball.px += ball.vx * deltaTime;
-            ball.py += ball.vy * deltaTime;
+        // Physics
+        for (auto &obj : objects) {
+            obj.ax = -obj.vx * 0.3f;
+            if (obj.py < window.getSize().y - pixelSize.y) {
+                obj.ay = gravity - obj.vy * 0.3f; // Include gravity
+            }else{
+                // Stop the object if it's at the bottom of the window
+                obj.vy = 0;
+                obj.py = window.getSize().y - pixelSize.y/2; // Adjust position to sit on the bottom
+            }
+            obj.vx += obj.ax * deltaTime;
+            obj.vy += obj.ay * deltaTime;
+            obj.px += obj.vx * deltaTime;
+            obj.py += obj.vy * deltaTime;
 
-            if(std::abs(ball.vx * ball.vx + ball.vy * ball.vy) <0.01f){ // If the velocity is < 0.01f set it to 0
-                ball.vx = 0;
-                ball.vy = 0;
+            if(std::abs(obj.vx * obj.vx + obj.vy * obj.vy) <0.001f){ // If the velocity is < 0.01f set it to 0
+                obj.vx = 0;
+                obj.vy = 0;
             }
 
         }
         for (auto &obj : objects) {
             for (auto &target : objects) {
                 if (&obj != &target) { // Check if they are different objects
-                    if (DoRectanglesOverlap(obj.px, obj.py, obj.w, obj.h,
-                                            target.px, target.py, target.w, target.h)) { // Collision
+                    if (DoRectanglesOverlap(obj.px, obj.py, obj.w, obj.h,target.px, target.py, target.w, target.h)) { // Collision
                         vecCollidingPairs.emplace_back(&obj, &target);
 
                         // Calculate overlap in both axes
@@ -139,16 +158,54 @@ int main()
                         }
 
                     }
+                    if(doGhostsOverlap(obj.px, obj.py, obj.w *3, obj.h *3,target.px, target.py)){
+                        obj.c = sf::Color::Red;
+                    }
+                    else{
+                        obj.c = sf::Color::White;
+                    }
                 }
             }
         }
-        for(auto c :vecCollidingPairs){
+        for (auto &obj : objects) {
+            obj.c = sf::Color::White;  // Reset color to white at the start
+
+            for (auto &target : objects) {
+                if (&obj != &target) { // Check if they are different objects
+                    if (doGhostsOverlap(obj.px, obj.py, obj.w * 2.99, obj.h * 2.99, target.px, target.py)) {
+                        obj.c = sf::Color::Red;  // Set color to red if overlapping
+                        break;  // Break out of the loop after finding the first overlap
+                    }
+                }
+            }
+        }
+        for (auto &obj : objects) {
+            sf::RectangleShape ghost;
+            float scaleFactor = 3.0f;
+            float newWidth = obj.w * scaleFactor;
+            float newHeight = obj.h * scaleFactor;
+            float offsetX = obj.w * (scaleFactor) / 2.0f;
+            float offsetY = obj.h * (scaleFactor) / 2.0f;
+            ghost.setPosition(obj.px - offsetX, obj.py - offsetY);
+            ghost.setSize(sf::Vector2f(newWidth, newHeight));
+            ghost.setFillColor(sf::Color(200, 200, 200, 20));
+
+
+            // Draw the ghost
+            window.draw(ghost);
+            drawRectangleGameObject(obj, obj.c); //Draw each
+        }
+
+        for (auto c :vecCollidingPairs){
             GameObject* obj1 = c.first;
             GameObject* obj2 = c.second;
             drawLineBetween(obj1->px, obj1->py, obj2->px, obj2->py, sf::Color::Red);
         }
-        for(auto &ball : objects){
-            drawRectangleGameObject(ball); //Draw each ball
+        for (GameObject& obj : objects) {
+            // Check if the object is not moving
+            if (obj.vx == 0 && obj.vy == 0) {
+                snapGameObjectToGrid(obj);
+            }
         }
         window.display();
     }
@@ -196,14 +253,14 @@ void handleMouseButtonReleased(const sf::Event& event){
         pSelectedObject = nullptr;
     }
 }
-void drawRectangleGameObject(GameObject& rect){
+void drawRectangleGameObject(GameObject& rect, sf::Color color){
     sf::RectangleShape rectangle;
     // Set the position considering px and py as the center
     rectangle.setPosition(rect.px - rect.w / 2.0f, rect.py - rect.h / 2.0f);
     rectangle.setSize(sf::Vector2f(rect.w, rect.h));
+    rectangle.setFillColor(color);
     window.draw(rectangle);
 }
-
 void drawLineBetween(float x1, float y1, float x2, float y2, sf::Color color){
     sf::Vertex line[] = {
             sf::Vertex(sf::Vector2f(x1, y1), color),
@@ -211,11 +268,23 @@ void drawLineBetween(float x1, float y1, float x2, float y2, sf::Color color){
     };
     window.draw(line, 2, sf::Lines); // Draw the line
 }
-void snapGameObjectToGrid(GameObject& obj, float gridSize){
-    // Adjust position to align to a grid
-    float adjustedX = std::floor(obj.px / gridSize) * gridSize;
-    float adjustedY = std::floor(obj.py / gridSize) * gridSize;
+void snapGameObjectToGrid(GameObject& obj) {
+    // Adjust position to align to the nearest grid spot
+    float adjustedX = std::round(obj.px / pixelSize.x) * pixelSize.x;
+    float adjustedY = std::round(obj.py / pixelSize.y) * pixelSize.y;
     obj.px = adjustedX;
     obj.py = adjustedY;
-}
 
+}
+bool doGhostsOverlap(float px, float py, float checkWidth, float checkHeight, float targetX, float targetY){
+    float left1 = px - checkWidth / 2.f;
+    float right1 = px + checkWidth / 2.f;
+    float top1 = py - checkHeight / 2.f;
+    float  bottom1 = py + checkHeight / 2.f;
+
+    float left2 = targetX - checkWidth / 2.f;
+    float right2 = targetX + checkWidth / 2.f;
+    float top2 = targetY - checkHeight / 2.f;
+    float  bottom2 = targetY + checkHeight / 2.f;
+    return !(left1 > right2 || right1 < left2 || top1 > bottom2 || bottom1 < top2);
+}
