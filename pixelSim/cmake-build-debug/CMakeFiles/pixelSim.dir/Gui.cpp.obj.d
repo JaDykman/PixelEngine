af35b0c01a5f01339d786bd508244cd78c9a2d1d
Jacob <jdykma80992@viterbo.edu>
2023-11-14 13:59:43 -0600

    Refactor

--- pixelSim/cmake-build-debug/CMakeFiles/pixelSim.dir/Gui.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pixelSim.dir/Gui.cpp.obj: \
- C:\Users\jacob\OneDrive\Documents\GitHub\PixelEngine\pixelSim\cmake-build-debug\Gui.cpp \
- C:\Users\jacob\OneDrive\Documents\GitHub\PixelEngine\pixelSim\cmake-build-debug\Gui.h
--- pixelSim/cmake-build-debug/CMakeFiles/pixelSim.dir/ObjectTypes/WaterObject.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pixelSim.dir/ObjectTypes/WaterObject.cpp.obj: \
- C:\Users\jacob\OneDrive\Documents\GitHub\PixelEngine\pixelSim\cmake-build-debug\ObjectTypes\WaterObject.cpp \
- C:\Users\jacob\OneDrive\Documents\GitHub\PixelEngine\pixelSim\cmake-build-debug\ObjectTypes\WaterObject.h
